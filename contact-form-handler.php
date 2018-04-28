cc3aa53c250fd00f98c9cebab83e903648438dfe
Jay Johnson <jaysbeverage@gmail.com>
2018-04-27 22:33:39 -0700

    email handle

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,46 +1,11 @@
-$errors = '';
-$myemail = 'jaysbeverage@gmail.com';
-if(empty($_POST['name'])  ||
-   empty($_POST['email']) ||
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-
+<?php
 $name = $_POST['name'];
-$email_address = $_POST['email'];
+$email = $_POST['email'];
 $message = $_POST['message'];
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-{
-
-$to = $myemail;
-
-$email_subject = "Contact form submission: $name";
-
-$email_body = "You have received a new message. ".
-
-" Here are the details:\n Name: $name \n ".
-
-"Email: $email_address\n Message \n $message";
-
-$headers = "From: $myemail\n";
-
-$headers .= "Reply-To: $email_address";
-
-mail($to,$email_subject,$email_body,$headers);
-
-//redirect to the 'thank you' page
-
-header('Location: contact-form-thank-you.html');
-
-
-25
-}
+$formcontent="From: $name \n Message: $message";
+$recipient = "jaysbeverage@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
