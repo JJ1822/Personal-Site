cf705631580721379125bf15ac84edecc938578f
Jay Johnson <jaysbeverage@gmail.com>
2018-04-27 20:53:48 -0700

    email handle

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,5 +1,5 @@
 $errors = '';
-$myemail = 'yourname@website.com';
+$myemail = 'jaysbeverage@gmail.com';
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['message']))
@@ -16,4 +16,31 @@ if (!preg_match(
 $email_address))
 {
     $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+
+$to = $myemail;
+
+$email_subject = "Contact form submission: $name";
+
+$email_body = "You have received a new message. ".
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the 'thank you' page
+
+header('Location: contact-form-thank-you.html');
+
+
+25
 }
